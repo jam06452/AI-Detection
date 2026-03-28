7ae14510dfc9b76ad12092df9bc941b53d714a36
jam06452 <jam@jam06452.uk>
2026-03-28 17:32:17 +0000

    Got a working PoC of it with chunking in elixir

--- lib/sifter/AI.ex
+++ lib/sifter/AI.ex
@@ -1,15 +1,34 @@
 defmodule Sifter.AI do
-  def test do
+  def test(text) do
+    chunks =
+      text
+      |> String.split(~r/\s+/, trim: true)
+      |> Enum.chunk_every(300, 150, [])
+      |> Enum.map(&Enum.join(&1, " "))
+
     Nx.global_default_backend(EXLA.Backend)
     {:ok, model_info} = Bumblebee.load_model({:hf, "followsci/bert-ai-text-detector"})
     {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "google-bert/bert-base-uncased"})
     serving = Bumblebee.Text.text_classification(model_info, tokenizer, top_k: nil)
 
-    text = "This is a test bit of text"
+    output = Nx.Serving.run(serving, chunks)
 
-    output = Nx.Serving.run(serving, text)
+    chunk_scores =
+      output
+      |> Enum.map(fn result ->
+        prediction = Enum.find(result.predictions, &(&1.label == "LABEL_1"))
+        score = Map.get(prediction, :score)
+        percent = Float.round(score * 100, 2)
 
-    ai_prediction = Enum.find(output.predictions, &(&1.label == "LABEL_1"))
-    "AI Probability: #{ai_prediction.score * 100}%"
+        percent
+      end)
+
+    total =
+      case chunk_scores do
+        [] -> 0.0
+        _ -> Float.round(Enum.sum(chunk_scores) / Enum.count(chunk_scores), 2)
+      end
+
+    List.to_tuple([total | chunk_scores])
   end
 end
