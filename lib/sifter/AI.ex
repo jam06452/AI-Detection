2f284c7dc8862eefeb7187cb7bdca97efc0689e8
jam06452 <danjward8@gmail.com>
2026-04-26 17:54:52 +0100

    Changed AI Model used to classification

--- lib/sifter/AI.ex
+++ lib/sifter/AI.ex
@@ -4,12 +4,15 @@ defmodule Sifter.AI do
 
     chunks =
       text
+      |> clean_text()
       |> String.split(~r/\s+/, trim: true)
       |> Enum.chunk_every(300, 150, [])
       |> Enum.map(&Enum.join(&1, " "))
 
-    {:ok, model_info} = Bumblebee.load_model({:hf, "followsci/bert-ai-text-detector"})
-    {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "google-bert/bert-base-uncased"})
+    {:ok, model_info} =
+      Bumblebee.load_model({:hf, "fakespot-ai/roberta-base-ai-text-detection-v1"})
+
+    {:ok, tokenizer} = Bumblebee.load_tokenizer({:hf, "FacebookAI/roberta-base"})
     serving = Bumblebee.Text.text_classification(model_info, tokenizer, top_k: nil)
 
     output = Nx.Serving.run(serving, chunks)
@@ -17,8 +20,7 @@ defmodule Sifter.AI do
     chunk_scores =
       output
       |> Enum.map(fn result ->
-        prediction = Enum.find(result.predictions, &(&1.label == "LABEL_1"))
-        score = Map.get(prediction, :score)
+        score = ai_score(result.predictions)
         percent = Float.round(score * 100, 2)
 
         percent
@@ -31,5 +33,18 @@ defmodule Sifter.AI do
       end
 
     %{total: total, chunk_scores: chunk_scores}
+  end
+
+  defp clean_text(text) do
+    text
+    |> String.replace(~r/\s+/, " ")
+    |> String.trim()
+  end
+
+  defp ai_score(predictions) do
+    case Enum.find(predictions, &(&1.label in ["AI", "LABEL_1"])) do
+      nil -> 0.0
+      prediction -> prediction.score
+    end
   end
 end
